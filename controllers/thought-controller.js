66a7bf72f2c6e85d2107085f11d40c43ccc5d4bc
Swagnarok <darryl.le.630@gmail.com>
2022-08-31 02:39:13 -0700

    final adjustments to code, finished reactions logic

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,90 +1,92 @@
-const { User, Thought, Reaction} = require('../models')
+const { User, Thought, Reaction } = require('../models')
 
 module.exports = {
+    // Grabbing all thoughts
     getThoughts(req, res) {
         Thought.find()
             .then((thoughts) => res.json(thoughts))
             .catch((error) => res.status(500).json(error))
-},
+    },
 
+    // Grabbing a single thought
     getOneThought(req, res) {
-        Thought.findOne({ _id: req.params.thoughtId})
+        Thought.findOne({ _id: req.params.thoughtId })
             .then((thoughts) =>
                 !thoughts
-                  ? res.status(404).json({ message: 'No thought here...' })
-                  : res.json(thoughts)
-             )
+                    ? res.status(404).json({ message: 'No thought here...' })
+                    : res.json(thoughts)
+            )
             .catch((error) => res.status(500).json(error));
-},
+    },
 
+    // Creating a thought
     createThought(req, res) {
         Thought.create(req.body)
             .then((thoughts) => {
                 User.findOneAndUpdate(
                     { username: thoughts.username },
-                    { $push: { thoughts: thoughts._id}},
+                    { $push: { thoughts: thoughts._id } },
                     { new: true }
                 )
-                .then(() => res.json({ message: `${thoughts.username} posted a thought!` }))
+                    .then(() => res.json({ message: `${thoughts.username} posted a thought!` }))
             })
             .catch((error) => res.status(500).json(error))
-},
+    },
 
+    // Updating a thought
     updateThought(req, res) {
         Thought.findOneAndUpdate(
             { _id: req.params.thoughtId },
             { $set: req.body },
-            { new: true}
+            { new: true }
         )
             .then((thoughts) =>
                 !thoughts
-                ? res.status(404).json({ message: 'No thought here...' })
-                : res.json(thoughts)
+                    ? res.status(404).json({ message: 'No thought here...' })
+                    : res.json(thoughts)
             )
             .catch((error) => res.status(500).json(error));
-},
+    },
 
+    // Deleting a thought
     deleteThought(req, res) {
         Thought.findOneAndDelete({ _id: req.params.thoughtId })
             .then((thoughts) =>
                 !thoughts
-                ? res.status(404).json({ message: 'No thought here' })
-                : res.json({ message: "Thought deleted!"})
+                    ? res.status(404).json({ message: 'No thought here' })
+                    : res.json({ message: "Thought deleted!" })
             )
             .catch((error) => res.status(500).json(error));
-},
+    },
 
-//     addReaction(req, res) {
-//         Reaction.create(req.body)
-//             .then((thoughts) => {
-//                 Thought.findOneAndUpdate(
-//                     { _id: req.params.thoughtId},
-//                     { $push: { reactions: req.body}},
-//                     { runValidators: true, new: true }
-//                 )
-//                 .then(() => res.json({ message: "Reaction added!" }))
-//             })
-//             .catch((error) => res.status(500).json(error))
-// },
-
+    // Adding a reaction to a thought
     addReaction(req, res) {
-        Thought.findOneAndUpdate({ _id: req.params.thoughtId}, {$push: { reactions: req.body}}, { new: true })
-            .then((thoughts) =>
-                !thoughts
-                ? res.status(404).json({ message: 'No thought here' })
-                : res.json(thoughts)
-            )
+        Reaction.create(req.body)
+            .then((reaction) => {
+                Thought.findOneAndUpdate(
+                    { _id: req.params.thoughtId },
+                    { $push: { reactions: reaction._id } },
+                    { runValidators: true, new: true }
+                )
+                    .then(() => res.json({ message: "Reaction added!" }))
+            })
             .catch((error) => res.status(500).json(error))
-},
+    },
 
+    // Deleting a reaction on a thought
     deleteReaction(req, res) {
-        Thought.findOneAndUpdate({ _id: req.params.thoughtId}, {$pull: { reactions: { _id: req.params.reactionId}}}, { new: true })
-        .then((thoughts) =>
-            !thoughts
-            ? res.status(404).json({ message: 'No thought here' })
-            : res.json(thoughts)
-        )
-        .catch((error) => res.status(500).json(error))
-}
+        Reaction.deleteOne({ _id: req.params.reactionId }).then(() => {
+            Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId }, 
+                { $pull: { reactions: req.params.reactionId } }, 
+                { new: true })
+                .then((thoughts) =>
+                    !thoughts
+                        ? res.status(404).json({ message: 'No thought here' })
+                        : res.json(thoughts)
+                )
+        })
+            .catch((error) => res.status(500).json(error))
+    }
 
 }
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,17 +1,23 @@
 const {User, Thought} = require("../models")
 
 module.exports = {
+    // Grabbing all users
     getUsers(req, res) {
         User.find()
+                // Including users' thoughts
                 .populate({ path: "thoughts", select: "-__v" })
+                // Including users' friends
                 .populate({ path: "friends", select: "-__v" })
             .then((users) => res.json(users))
             .catch((error) => res.status(500).json(error))
     },
 
+    // Grabbing a single user
     getOneUser(req, res) {
         User.findOne({ _id: req.params.id})
+                // Including user's thoughts
                 .populate({ path: "thoughts", select: "-__v" })
+                // Including user's friends
                 .populate({ path: "friends", select: "-__v" })
             .then((users) =>
                 !users
@@ -21,12 +27,14 @@ module.exports = {
               .catch((error) => res.status(500).json(error));
     },
 
+    // Creating a user
     createUser(req, res) {
         User.create(req.body)
             .then((users) => res.json(users))
             .catch((error) => res.status(500).json(error))
     },
 
+    // Updating a user
     updateUser(req, res) {
         User.findOneAndUpdate(
             { _id: req.params.id },
@@ -41,17 +49,20 @@ module.exports = {
             .catch((error) => res.status(500).json(error));
     },
 
+    // Deleting a user
     deleteUser(req, res) {
         User.findOneAndDelete({ _id: req.params.id })
             .then((users) =>
                 !users
                   ? res.status(404).json({ message: 'No user with that ID' })
+                  // Deletes user's thoughts as well when user is deleted
                   : Thought.deleteMany({ _id: { $in: users.thoughts }})
             )
             .then(() => res.json({ message: "User and thoughts deleted!"}))
             .catch((error) => res.status(500).json(error));
     },
 
+    // Adding a friend to a user's friend list
     addFriend(req, res) {
         User.findOneAndUpdate({ _id: req.params.id}, {$push: { friends: req.params.friendId}}, { new: true})
             .then((users) =>
@@ -62,6 +73,7 @@ module.exports = {
             .catch((error) => res.status(500).json(error))
     },
 
+    // Deleting a friend from a user's friend list
     deleteFriend(req, res) {
         User.findOneAndUpdate({ _id: req.params.id}, {$pull: { friends: req.params.friendId}}, { new: true})
         .then((users) =>
